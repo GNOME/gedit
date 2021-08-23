ced9728fc2a066ac0d7bdc2020add2a95d3e948c
Chun-wei Fan <fanchunwei@src.gnome.org>
2021-08-23 18:21:02 +0800

    Add gedit-macros.h
    This adds a header so that we define a macro that can be overridden to
    export symbols using compiler directives.

---
+++ gedit/gedit-macros.h
@@ -0,0 +1,32 @@
+/*
+ * gedit-utils.h
+ * This file is part of gedit
+ *
+ * Copyright (C) 1998, 1999 Alex Roberts, Evan Lawrence
+ * Copyright (C) 2000, 2001 Chema Celorio, Paolo Maggi
+ * Copyright (C) 2002 - 2005 Paolo Maggi
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GEDIT_MACROS_H
+#define GEDIT_MACROS_H
+
+#ifndef GEDIT_API
+#define GEDIT_API
+#endif
+
+#endif /* GEDIT_MACROS_H */
+
+/* ex:set ts=8 noet: */
