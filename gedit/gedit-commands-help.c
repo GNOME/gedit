8f76ead80f4d9b77c080781acf9469e31f110d73
Jeremy Bicha <jeremy.bicha@canonical.com>
2022-03-15 08:18:27 -0400

    Restore the classic gedit branding to the about screen
    Revert "About dialog: have the gedit icon, not the old logo"
    
    This reverts commit d44b527c1335a1ac050c7e36e72a12b0f7285eb7.

--- gedit/gedit-commands-help.c
+++ gedit/gedit-commands-help.c
@@ -102,7 +102,17 @@ _gedit_cmd_help_about (GeditWindow *window)
 		NULL
 	};
 
+	GdkPixbuf *logo;
+	GError *error = NULL;
+
 	gedit_debug (DEBUG_COMMANDS);
+
+	logo = gdk_pixbuf_new_from_resource ("/org/gnome/gedit/pixmaps/gedit-logo.png", &error);
+	if (error != NULL)
+	{
+		g_warning ("Error when loading the gedit logo: %s", error->message);
+		g_clear_error (&error);
+	}
 
 	gtk_show_about_dialog (GTK_WINDOW (window),
 			       "program-name", "gedit",
@@ -110,13 +120,15 @@ _gedit_cmd_help_about (GeditWindow *window)
 			       "comments", _("gedit is a small and lightweight text editor for the GNOME desktop"),
 			       "copyright", "Copyright 1998-2021 – the gedit team",
 			       "license-type", GTK_LICENSE_GPL_2_0,
-			       "logo-icon-name", "org.gnome.gedit",
+			       "logo", logo,
 			       "documenters", documenters,
 			       "translator-credits", _("translator-credits"),
 			       "version", VERSION,
 			       "website", "http://www.gedit.org",
 			       "website-label", "www.gedit.org",
 			       NULL);
+
+	g_clear_object (&logo);
 }
 
 /* ex:set ts=8 noet: */
