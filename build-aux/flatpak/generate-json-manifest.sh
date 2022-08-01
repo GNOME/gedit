ebd5fa06a51278cf353b7fda0ee8cc3ed7364229
Sébastien Wilmet <swilmet@informatique-libre.be>
2022-08-01 20:58:47 +0200

    flatpak: update/improve some comments

--- build-aux/flatpak/generate-json-manifest.sh
+++ build-aux/flatpak/generate-json-manifest.sh
@@ -5,4 +5,7 @@
 # yq: A portable command-line YAML processor.
 # Currently hosted here: https://github.com/mikefarah/yq
 
+# At the time of writing, some tools around Flatpak (for example gnome-builder)
+# only supports the *.json format, but *.yml is easier to edit.
+
 yq eval 'org.gnome.gedit.yml' --output-format json > 'org.gnome.gedit.json'
