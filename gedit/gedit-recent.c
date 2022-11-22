ce0bac9822bdb64f4d0269d09ab016b5a7ba43ea
Sébastien Wilmet <swilmet@informatique-libre.be>
2022-11-22 16:00:00 +0100

    recent: some thoughts for gedit_recent_remove_if_local()

--- gedit/gedit-recent.c
+++ gedit/gedit-recent.c
@@ -66,16 +66,34 @@ gedit_recent_add_document (GeditDocument *document)
 	g_free (recent_data.mime_type);
 }
 
+/* If a file is local, chances are that if load/save fails the file has been
+ * removed and the failure is permanent so we remove it from the list of recent
+ * files. For remote files the failure may be just transitory and we keep the
+ * file in the list.
+ *
+ * FIXME: for files coming from external disks, USB keys etc, if the external
+ * storage device is not mounted, do not remove the file from the list. On the
+ * other hand, if the external storage device is mounted and the file isn't
+ * there, remove it from the list.
+ *
+ * FIXME: for remote files, perhaps do the same as for external storage devices,
+ * IF the connection to the server succeeds (the directory is there).
+ *
+ * FIXME: for the above FIXMEs, we should probably rely (in part) on the GError
+ * we receive.
+ * And if it was a file loading, a useful information to have is whether the
+ * file was opened from the list of recent files. If it was *not*, the user can
+ * also just navigate again through the file hierarchy to re-open it later if
+ * the file comes back.
+ *
+ * IDEA: or just never remove files from the recent list, and call it the
+ * Recent History or something like that.
+ */
 void
 gedit_recent_remove_if_local (GFile *location)
 {
 	g_return_if_fail (G_IS_FILE (location));
 
-	/* If a file is local chances are that if load/save fails the file has
-	 * beed removed and the failure is permanent so we remove it from the
-	 * list of recent files. For remote files the failure may be just
-	 * transitory and we keep the file in the list.
-	 */
 	if (g_file_has_uri_scheme (location, "file"))
 	{
 		GtkRecentManager *recent_manager;
